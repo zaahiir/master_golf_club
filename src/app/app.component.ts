c1980f4ac924b9f49b1463fe459a356861f05c24
zaahiir <smartxoft@outlook.com>
2024-11-07 18:11:20 +0530

    feat
    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoadingBarModule} from "@ngx-loading-bar/core";
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingBarModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
