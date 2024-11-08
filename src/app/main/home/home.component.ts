c7afe5a7252532da11de914e395252444fcd68c0
zaahiir <smartxoft@outlook.com>
2024-11-08 20:28:49 +0530

    feat
    update courosel

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -30,19 +30,37 @@ export class HomeComponent {
     }
   ];
 
-  carouselImages = [
-    'https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-    'https://images.unsplash.com/photo-1552083375-1447ce886485?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-    'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+  carouselSlides = [
+    {
+      image: 'https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Explore Our Destination',
+      description: 'Discover the beauty and tranquility of our resort.',
+      buttonText: 'Learn More',
+      link: '#'
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1552083375-1447ce886485?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Upcoming Events',
+      description: 'Join us for our exciting golf tournaments and activities.',
+      buttonText: 'View Events',
+      link: '#'
+    },
+    {
+      image: 'https://images.unsplash.com/photo-1502759683299-cdcd6974244f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Become a Member',
+      description: 'Enjoy exclusive benefits and privileges as a member.',
+      buttonText: 'Join Now',
+      link: '#'
+    }
   ];
 
   currentSlide = 0;
 
   nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.carouselImages.length;
+    this.currentSlide = (this.currentSlide + 1) % this.carouselSlides.length;
   }
 
   prevSlide() {
-    this.currentSlide = (this.currentSlide - 1 + this.carouselImages.length) % this.carouselImages.length;
+    this.currentSlide = (this.currentSlide - 1 + this.carouselSlides.length) % this.carouselSlides.length;
   }
 }
