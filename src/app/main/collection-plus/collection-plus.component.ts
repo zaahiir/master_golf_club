63b2bc883daa4972bcc7d1ee5b63d463335ff29a
zaahiir <smartxoft@outlook.com>
2024-11-13 13:34:16 +0530

    feat
    update collection plus page bg-image

--- src/app/main/collection-plus/collection-plus.component.ts
+++ src/app/main/collection-plus/collection-plus.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-collection-plus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './collection-plus.component.html',
   styleUrl: './collection-plus.component.css'
 })
 export class CollectionPlusComponent {
+  cards = [
+    {
+      image: 'assets/card1.jpg',
+      title: 'Golf Lessons',
+    },
+    {
+      image: 'assets/card2.jpg',
+      title: 'Golf Events',
+    },
+    {
+      image: 'assets/card3.jpg',
+      title: 'Membership',
+    }
+  ];
 
 }
