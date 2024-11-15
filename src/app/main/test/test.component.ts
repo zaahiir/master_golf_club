671b7716885a4fb48a053be183aef499bdc601bb
zaahiir <smartxoft@outlook.com>
2024-11-15 18:23:32 +0530

    feat
    update

--- src/app/main/test/test.component.ts
+++ src/app/main/test/test.component.ts
@@ -6,9 +6,8 @@ import { faClock, faUsers, faChartBar, faBox } from '@fortawesome/free-solid-svg
 interface GolfAmenity {
   id: number;
   title: string;
-  value: string;
   tooltip: string;
-  icon: any;  // Changed to accept FA icon
+  icon: any; 
 }
 
 @Component({
@@ -19,9 +18,7 @@ interface GolfAmenity {
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  description: string = 'Your One Golf Club membership entitles you to free access to the OGC Collection of golf courses listed below (subject to tee time availability, handicap restrictions, time restrictions and our fair use policy on premium courses).';
-
-  // Define icons
+  // Define icons 
   clockIcon = faClock;
   usersIcon = faUsers;
   chartIcon = faChartBar;
@@ -31,28 +28,36 @@ export class TestComponent {
     {
       id: 1,
       title: 'No. of Tees per Day',
-      value: 'Unlimited access',
       tooltip: 'Number of tee times available per day',
       icon: this.clockIcon
     },
     {
       id: 2,
       title: 'Guest Restrictions',
-      value: 'Check policy details',
       tooltip: 'Guest policy information',
       icon: this.usersIcon
     },
     {
       id: 3,
       title: 'Minimum Handicap',
-      value: '24',
       tooltip: 'Required minimum handicap',
       icon: this.chartIcon
     },
     {
       id: 18,
       title: 'Club Storage',
-      value: 'Available',
+      tooltip: 'Equipment storage service',
+      icon: this.boxIcon
+    },
+    {
+      id: 3,
+      title: 'Minimum Handicap',
+      tooltip: 'Required minimum handicap',
+      icon: this.chartIcon
+    },
+    {
+      id: 18,
+      title: 'Club Storage',
       tooltip: 'Equipment storage service',
       icon: this.boxIcon
     }
